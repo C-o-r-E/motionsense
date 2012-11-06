76ff45460a5f8cd078cfea66a98110498a9e88a3
Corey Clayton <corey@Ayanami.local>
2012-11-05 21:47:47 -0500

    Initial Commit

---
+++ motionsense/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  motionsense
+//
+//  Created by Corey Clayton on 2012-11-05.
+//  Copyright (c) 2012 Awake Coding. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
